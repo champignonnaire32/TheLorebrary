1bcf6385bc161c97de613e24186164869202fca7
Patrick Flynn <flynnp@FlynnPatrick.penntrafford.org>
2026-05-01 08:21:55 -0400

    chore: update config URLs to TheLorebrary

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://project-panic-catchup.vercel.app',
+  site: 'https://thelorebrary.vercel.app',
   integrations: [react(), sitemap()],
 
   vite: {
